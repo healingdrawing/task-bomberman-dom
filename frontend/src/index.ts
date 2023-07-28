984129c112be9f1f435c9ef0d8bc2b079d950da9
healingdrawing <healingdrawing@gmail.com>
2023-07-28 05:18:22 +0300

    wip refactoring: - add styles.css - change dev run command - remove artefacts from code - use mini-framework for one button (hope it is enough)

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,192 +1,27 @@
 import { router, store, events } from './framework/framework';
-import { todoService, Todo } from './todo_mvc/todo';
 
-// Add state to handle the current filter ("all", "active", "completed")
-store.setState({ filter: 'all' });
+function connect_to_game(event: Event) {
+  event.preventDefault(); // Prevent the default form submission
 
-// Add a utility function to get filtered todos based on the current filter
-function getFilteredTodos(): Todo[] {
-  const filter = store.getState().filter;
-  if (filter === 'active') {
-    return todoService.getTodos().filter((todo) => !todo.completed);
-  } else if (filter === 'completed') {
-    return todoService.getTodos().filter((todo) => todo.completed);
-  }
-  return todoService.getTodos();
-}
+  const inputField = document.getElementById("nickname") as HTMLInputElement;
 
-// Display todo's list on the screen
-function renderTodos() {
-
-  // Display clear completed button if there are completed todos
-  const clearCompletedButton = document.getElementById('clear-completed-button');
-  if (clearCompletedButton) {
-    const completedCount = todoService.getCompletedCount();
-    if (completedCount > 0) {
-      clearCompletedButton.style.display = 'inline-block';
-    } else {
-      clearCompletedButton.style.display = 'none';
-    }
+  if (!inputField.checkValidity()) {
+    // Display an error message or take appropriate action
+    alert(`🤦 Before play this game you should learn how to properly type on keyboard.
+    Please leave this place, it can be too danger for your virgin brain,
+    which is not able to choose nickname correctly.`);
+    return;
   }
 
-  // Display footer if there are todos
-  const all_todos = todoService.getTodos();
-  const footer = document.querySelector("footer") as HTMLElement;
-  if (all_todos.length > 0) {
-    footer.style.display = "block";
-  } else {
-    footer.style.display = "none";
-  }
+  const inputValue = inputField.value;
 
-  const counted_todos = todoService.getTodos().filter((todo) => !todo.completed);
-  const todoCounter = document.getElementById('todo-counter');
-  if (todoCounter) todoCounter.innerText = `${counted_todos.length} items left`;
-
-  const todos = getFilteredTodos();
-
-  const todoList = document.getElementById('todo-list');
-  if (todoList) {
-    todoList.innerHTML = '';
-    todos.forEach((todo) => {
-      const todoItem = document.createElement('li');
-      todoItem.className = 'todo-item';
-
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.checked = todo.completed;
-
-      events.on('change', checkbox, () => {
-        todoService.toggleCompleted(todo.id);
-        updateToggleAllCheckbox();
-      });
-      todoItem.appendChild(checkbox);
-
-      const textInput = document.createElement('input');
-      textInput.type = 'text';
-      textInput.value = todo.text;
-      textInput.readOnly = true;
-
-      events.on('dblclick', textInput, () => {
-        textInput.readOnly = false;
-      });
-
-      events.on('blur', textInput, () => {
-        todoService.editTodo(todo.id, textInput.value);
-        textInput.readOnly = true;
-      });
-
-      todoItem.appendChild(textInput);
-
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Delete';
-
-      events.on('click', deleteButton, () => {
-        todoService.removeTodo(todo.id);
-        updateToggleAllCheckbox();
-      });
-
-      todoItem.appendChild(deleteButton);
-
-      todoList.appendChild(todoItem);
-    });
-  }
+  // Process the input value
+  alert("Input value: " + inputValue);
 }
 
-// Add event listener for toggle all checkbox
-const toggleAllCheckbox = document.getElementById('toggle-all-checkbox') as HTMLInputElement;
+/**initialization */
+(() => {
+  const connect_to_game_button = document.getElementById("connect_to_game") as HTMLButtonElement;
+  events.on("click", connect_to_game_button, connect_to_game); // usage of mini-framework 🙂 mission complete
 
-let updatingToggleAll = false; // Flag to prevent change event from individual todos affecting toggleAllCheckbox
-
-// Function to check/uncheck all todo checkboxes
-function toggleAllTodos(checked: boolean) {
-  const todos = todoService.getTodos();
-  todos.forEach((todo) => (todo.completed = checked));
-  updatingToggleAll = true; // Set the flag to true to prevent change event from affecting toggleAllCheckbox
-  toggleAllCheckbox.checked = checked;
-  updatingToggleAll = false; // Reset the flag to false after updating toggleAllCheckbox
-  store.setState({ todos });
-}
-
-if (toggleAllCheckbox) {
-  events.on('change', toggleAllCheckbox, () => {
-    if (!updatingToggleAll) {
-      const checked = toggleAllCheckbox.checked;
-      toggleAllTodos(checked);
-    }
-  });
-}
-
-// Function to check the state of individual todo checkboxes and update the toggleAllCheckbox accordingly
-function updateToggleAllCheckbox() {
-  const todos = todoService.getTodos();
-  const allChecked = todos.every((todo) => todo.completed);
-  updatingToggleAll = true;
-  toggleAllCheckbox.checked = allChecked;
-  updatingToggleAll = false;
-}
-
-// Add todo to list (html "input" type=text, which add new todo after press enter)
-const addTodoInput = document.getElementById('add-todo-input') as HTMLInputElement;
-if (addTodoInput) {
-
-  events.on('keypress', addTodoInput, (event: { key: string; }) => {
-    if (event.key === 'Enter') {
-      const text = addTodoInput.value;
-      if (text.trim() !== '') {
-        todoService.addTodo(text);
-        addTodoInput.value = '';
-      }
-      updateToggleAllCheckbox();
-    }
-  });
-}
-
-// Add event listeners for filter buttons
-const allButton = document.getElementById('all-button');
-const activeButton = document.getElementById('active-button');
-const completedButton = document.getElementById('completed-button');
-if (allButton && activeButton && completedButton) {
-
-  events.on('click', allButton, () => {
-    router.navigate('/');
-  });
-
-  events.on('click', activeButton, () => {
-    router.navigate('/active');
-  });
-
-  events.on('click', completedButton, () => {
-    router.navigate('/completed');
-  });
-
-}
-
-// add event listener for clear completed button
-const clearCompletedButton = document.getElementById('clear-completed-button');
-if (clearCompletedButton) {
-  events.on('click', clearCompletedButton, () => {
-    todoService.clearCompleted();
-  });
-}
-
-// Subscribe to state changes and update the UI accordingly
-store.subscribe(renderTodos);
-
-// Initial render
-renderTodos();
-
-// Abstracting the DOM Routing System
-router.addRoute('/', () => {
-  store.setState({ filter: 'all' });
-});
-
-router.addRoute('/active', () => {
-  store.setState({ filter: 'active' });
-});
-
-router.addRoute('/completed', () => {
-  store.setState({ filter: 'completed' });
-});
-
-// Start the router
-router.navigate(window.location.pathname);
+})()
