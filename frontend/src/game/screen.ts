a349b0e92ee1356d3980c80fb4ed5e84c55be907
healingdrawing <healingdrawing@gmail.com>
2023-08-03 19:06:57 +0300

    refactor screen.ts

--- frontend/src/game/screen.ts
+++ frontend/src/game/screen.ts
@@ -1,6 +1,21 @@
 import { screen_prepare } from "./screen_prepare"
 
 class GameScreen {
+
+  weak_obstacles: Map<string, HTMLDivElement>
+  power_ups: Map<string, HTMLDivElement>
+  bombs: Map<string, HTMLDivElement>
+  players: Map<string, HTMLDivElement>
+  explosions: Map<string, HTMLDivElement>
+
+  constructor() {
+    this.weak_obstacles = screen_prepare.weak_obstacles
+    this.power_ups = screen_prepare.power_ups
+    this.bombs = screen_prepare.bombs
+    this.players = screen_prepare.players
+    this.explosions = screen_prepare.explosions
+  }
+
   /**generates all required objects to display the game and place them on screen*/
   async prepare() {
     await screen_prepare.prebuild_game_field()
