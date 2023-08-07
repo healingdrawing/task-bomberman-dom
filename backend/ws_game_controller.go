8615f18d11710a7ac539ecfc9a299e8731437508
healingdrawing <healingdrawing@gmail.com>
2023-08-07 07:21:59 +0300

    wip player moving

--- backend/ws_game_controller.go
+++ backend/ws_game_controller.go
@@ -43,6 +43,13 @@ var move_cps = map[bool]int64{
 	false: 1,
 }
 
+func unpress_all_arrows(player PLAYER) {
+	player.up_pressed = false
+	player.down_pressed = false
+	player.left_pressed = false
+	player.right_pressed = false
+}
+
 func unpress_other_arrows(player PLAYER, control string) {
 	switch control {
 	case string(WS_UP_ON):
@@ -90,15 +97,12 @@ func ws_up_handler(number int, control string) {
 		return
 	}
 
-	if !player.up_pressed {
-		return
-	}
-
 	//unix time stamp in milliseconds
 	unix_ts := time.Now().UnixNano()
 
 	if !player.moving {
-		target_position_cell := fmt.Sprint(player.X) + fmt.Sprint(player.Y-1)
+		// check if player can move up
+		target_position_cell := fmt.Sprintf("%d%d", player.X, player.Y-1)
 		if _, ok := game.free_cells[target_position_cell]; !ok {
 			return
 		}
@@ -108,25 +112,45 @@ func ws_up_handler(number int, control string) {
 		// time in nanoseconds
 		one_cell_move_duration := oneSecond.Nanoseconds() / move_cps[player.Turbo]
 		player.one_cell_move_duration = one_cell_move_duration
-		player.moving_start_time_stamp = time.Now().UnixNano()
+		player.moving_start_time_stamp = unix_ts
 		player.Target_y = player.Y - 1
 		player.moving = true
 		game.Players[pn[number]] = player
 		//todo: send to all clients the command to move player. from present position to target position
 		time.Sleep(time.Duration(one_cell_move_duration) / 10 * 8 * time.Nanosecond)
 		ws_up_handler(number, control)
+		return
 	} else if player.moving &&
+		player.one_cell_move_duration < unix_ts-player.moving_start_time_stamp {
+		// check if moving completed
+		player.moving = false
+		player.Y = player.Target_y
+		game.Players[pn[number]] = player
+		//todo: send to all clients the command to stand player on target position
+		return
+	} else if player.moving &&
+		player.up_pressed &&
 		player.Target_y == player.Y-1 &&
-		unix_ts-player.moving_start_time_stamp > player.one_cell_move_duration/10*8-2 {
+		player.one_cell_move_duration/10*8-100000 < unix_ts-player.moving_start_time_stamp {
 		// check if 80% of timer is done, and player still wants to move up
 		// if yes, then move/aim player up one cell more if possible
-		target_position_cell := fmt.Sprint(player.X) + fmt.Sprint(player.Y-2)
+		// 100000 is 100 microseconds gap to wait while after timesleep is over, recursive call is done
+		target_position_cell := fmt.Sprintf("%d%d", player.X, player.Y-2)
 		if _, ok := game.free_cells[target_position_cell]; !ok {
+			time.Sleep(time.Duration(player.one_cell_move_duration) / 100 * time.Nanosecond)
+			ws_up_handler(number, control)
 			return
 		}
 		player.Target_y = player.Y - 2
 		player.moving_start_time_stamp += player.one_cell_move_duration
+		game.Players[pn[number]] = player
 		//todo: send to all clients the command to move player. from present position to target position
+		time.Sleep(time.Duration(player.one_cell_move_duration) * 120 / 100 * time.Nanosecond)
+		ws_up_handler(number, control)
+	} else {
+		time.Sleep(time.Duration(player.one_cell_move_duration) / 100 * time.Nanosecond)
+		ws_up_handler(number, control)
+		return
 	}
 
 }
