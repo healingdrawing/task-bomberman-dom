a8a8a1281f62e2b724b1738411e878db951c25eb
healingdrawing <healingdrawing@gmail.com>
2023-08-11 10:01:29 +0300

    ws_game.go game_debug = false Docker works. waiting.go timers still on 1 sec both. Change to 10 20 respectively. Code not cleaned. Clean/refactor backend/frontend log/console.log

--- backend/ws_game_control_power_up.go
+++ backend/ws_game_control_power_up.go
@@ -7,10 +7,6 @@ import (
 
 // "bombs_max++", "explosion_range++", "turbo boolean/move faster" is "1" "2" "3" respectively
 func check_power_up(player *PLAYER) {
-	// check if player is on power up cell
-	// if yes, then apply power up, and hide power up
-	// if no, then do nothing
-
 	power_up_to_delete := "" // xy cell indices
 	effect := ""             // "1" "2" "3" respectively , to remove css class animation
 
@@ -19,7 +15,7 @@ func check_power_up(player *PLAYER) {
 		power_up := value.(POWER_UP)
 		player_xy := fmt.Sprintf("%d%d", player.X, player.Y)
 		target_xy := fmt.Sprintf("%d%d", player.Target_x, player.Target_y)
-		if power_up.Show && player_xy == xy && target_xy == xy {
+		if power_up.Show && player_xy == xy && target_xy == xy { // stop to take power up
 			switch power_up.Effect {
 			case "1":
 				player.bombs_max++
@@ -28,7 +24,7 @@ func check_power_up(player *PLAYER) {
 			case "3":
 				player.Turbo = !player.Turbo
 			}
-			// power_up.Show = false
+			// power_up.Show = false // looks like not needed because deletion is below
 			if power_up_to_delete == "" {
 				power_up_to_delete = xy
 				effect = power_up.Effect
